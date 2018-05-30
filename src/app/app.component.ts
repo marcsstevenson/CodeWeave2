71b38fda4187bdb6bfb3a981d85c8d75adc4f231
Marc Stevenson <marc@moola.co.nz>
2018-05-31 08:08:00 +1200

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cw-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'cw';
+}
